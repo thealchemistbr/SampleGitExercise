52152610bc32af27acd8f892882218f01f4eca27
Marcelo N. Magri <morbidsoftware@gmail.com>
2015-07-22 07:37:35 -0300

    Initial commit, adding Math Lib and Console App

---
+++ SampleGitExercise/Mat/MathConsole.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Math;
+
+namespace Mat
+{
+    class MathConsole
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Math Console 1.0");
+            Console.WriteLine();
+
+            Console.WriteLine("Add: ", Operations.Add(1, 2));
+            Console.WriteLine("Subtract: ", Operations.Subtract(1, 2));
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SampleGitExercise/Math/Operations.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Math
+{
+    public class Operations
+    {
+        /// <summary>
+        /// Returns the sum os two integers
+        /// </summary>
+        /// <param name="a">first value</param>
+        /// <param name="b">second value</param>
+        /// <returns></returns>
+        public static int Add (int a, int b)
+        {
+            return a + b;
+        }
+
+        /// <summary>
+        /// Returns the subtraction of two integers
+        /// </summary>
+        /// <param name="a">first value</param>
+        /// <param name="b">second value</param>
+        /// <returns></returns>
+        public static int Subtract (int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
